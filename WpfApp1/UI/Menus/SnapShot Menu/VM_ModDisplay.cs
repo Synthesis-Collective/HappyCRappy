41f342f9e778b5e5b07b7820fb6506b459fe4b45
Piranha91 <piranha9191@gmail.com>
2024-01-07 01:15:47 -0800

    Added per-snapshot stats upon snapshot load

--- WpfApp1/UI/Menus/SnapShot Menu/VM_ModDisplay.cs
+++ WpfApp1/UI/Menus/SnapShot Menu/VM_ModDisplay.cs
@@ -1,4 +1,5 @@
 using Noggog;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -11,8 +12,10 @@ namespace HappyCRappy;
 public class VM_ModDisplay : VM
 {
     public delegate VM_ModDisplay Factory(ModSnapshot selectedSnapshot, ModSnapshot currentSnapshot, Dictionary<string, List<PotentialConflictFinder.PotentialConflictRecord>> potentialConflicts);
-    public VM_ModDisplay(ModSnapshot selectedSnapshot, ModSnapshot currentSnapshot, Dictionary<string, List<PotentialConflictFinder.PotentialConflictRecord>> potentialConflicts, VM_RecordCategoryDisplay.Factory snapshotGroupFactory)
+    public VM_ModDisplay(ModSnapshot selectedSnapshot, ModSnapshot currentSnapshot, Dictionary<string, List<PotentialConflictFinder.PotentialConflictRecord>> potentialConflicts, VM_RecordCategoryDisplay.Factory snapshotGroupFactory, VM_SnapshotMenu snapshotMenu)
     {
+        _snapshotMenu = snapshotMenu;
+
         SelectedSnapshot = selectedSnapshot;
         CurrentSnapShot = currentSnapshot;
         DateTaken = selectedSnapshot.DateTaken;
@@ -43,18 +46,74 @@ public class VM_ModDisplay : VM
             }
             var categoryVM = snapshotGroupFactory(category, pairedSelectedCurrentSnapshots, conflictsInCategory);
             RecordCategories.Add(categoryVM);
+            NumConflict += categoryVM.NumConflict;
+            NumNonConflict += categoryVM.NumNonConflict;
+            NumPotentialConflict += categoryVM.NumPotentialConflict;
         }
 
         HasDifference = RecordCategories.Where(x => x.HasDifference).Any();
+
+        ShowPotentialConflictCount = _snapshotMenu.ShowPotentialConflicts;
+        this.WhenAnyValue(x => x._snapshotMenu.ShowPotentialConflicts)
+            .Subscribe(val => ShowPotentialConflictCount = val)
+            .DisposeWith(this);
+
+        this.WhenAnyValue(x => x.SelectedNode).Subscribe(y =>
+        {
+            if (y == null)
+            {
+                ShowStats = true;
+            }
+            else
+            {
+                ShowStats = false;
+            }
+        }).DisposeWith(this);
+
+        if (NumNonConflict == 1)
+        {
+            NonConflictStr = "record identical to Snapshot";
+        }
+        else
+        {
+            NonConflictStr = "records identical to Snapshot";
+        }
+
+        if (NumPotentialConflict == 1)
+        {
+            PotentialConflictStr = "record which is not a member of the displayed Conflict Resolution mod, but which may need Conflict Resolution";
+        }
+        else
+        {
+            PotentialConflictStr = "records which are not members of the displayed Conflict Resolution mod, but which may need Conflict Resolution";
+        }
+
+        if (NumConflict == 1)
+        {
+            ConflictStr = "record with differences from Snapshot";
+        }
+        else
+        {
+            ConflictStr = "records with differences from Snapshot";
+        }
     }
 
     public ModSnapshot SelectedSnapshot { get; set; }
     public ModSnapshot CurrentSnapShot { get; set; }
     public DateTime DateTaken { get; set; }
-    public ObservableCollection<ISnapshotDisplayNode> RecordCategories { get; set; } = new();
+    public ObservableCollection<VM_RecordCategoryDisplay> RecordCategories { get; set; } = new();
     public ISnapshotDisplayNode? SelectedNode { get; set; }
     public string DateTakenStr => ToLabelString(DateTaken);
     public bool HasDifference { get; set; } = false;
+    public int NumNonConflict { get; set; } = 0;
+    public string NonConflictStr { get; set; }
+    public int NumConflict { get; set; } = 0;
+    public string ConflictStr { get; set; }
+    public int NumPotentialConflict { get; set; } = 0;
+    public string PotentialConflictStr { get; set; }
+    public bool ShowPotentialConflictCount { get; set; } = false;
+    public bool ShowStats { get; set; } = false;
+    private readonly VM_SnapshotMenu _snapshotMenu;
 
     public static string ToLabelString(DateTime timestamp)
     {
